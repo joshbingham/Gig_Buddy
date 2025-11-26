85dfa33f3a7f8d5eac5f30e1ce05da340aa698f3
Josh Bingham <joshuabingham@hotmail.co.uk>
2025-11-26 10:50:56 +0000

    Set up firebase.js file

---
+++ server/test-connection.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+console.log('Testing connection to Supabase database...');
+client.connect()
+  .then(() => {
+    console.log('SUCCESS: Connected successfully!');
+    return client.query('SELECT NOW() as current_time');
+  })
+  .then(res => {
+    console.log('Current database time:', res.rows[0].current_time);
+  })
+  .catch(err => {
+    console.error('FAILED: Connection error:', err.message);
+  })
+  .finally(() => {
+    client.end();
+  });
